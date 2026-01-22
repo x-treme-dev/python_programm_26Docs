89d7a2c3e5556fadbeceb5496f47591e523f8a5c
extreme-engineer <newgalaxy401@gmail.com>
2026-01-22 17:30:07 +0300

    add func search_pdf

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ from kivy.uix.textinput import TextInput
 from kivy.core.window import Window
 import tkinter as tk
 from tkinter import filedialog
+import os
+import PyPDF2
 
-# Глобальная переменная для хранения пути
 directory_path_source = ''
+directory_path_target = ''
+
 # Установка размера окна
 Window.size = (500, 600)
+
 
 class MyApp(App):
     def build(self):
@@ -26,47 +30,49 @@ class MyApp(App):
         # Первая строка: метка и кнопка
         self.lb_source_choice = Label(text='Исходная директория:')
         btn_source_directory = Button(text='Выбрать')
-        btn_source_directory.bind(on_press=self.choose_directory)
+        btn_source_directory.bind(on_press=self.choose_source_directory)
         # Вторая строка: 2 метки 
-        self.lb_path = Label(text='')
-        self.lb_source_path = Label(text='')
+        self.lb_source_empty = Label(text='Выбрано:')
+        self.lb_source_path = Label(text='0')
         # Третья строка: метка и кнопка
         self.lb_target_choice = Label(text='Целевая директория:')
         btn_target_directory = Button(text='Выбрать')
-        #btn_directory.bind(on_press=self.choose_directory)
+        btn_target_directory.bind(on_press=self.choose_target_directory)
+        # Чертвертая строка: 2 метки
+        self.lb_target_empty = Label(text='Выбрано:')
+        self.lb_target_path = Label(text='0')
+        # Пятная строка: метка и кнопка
+        self.lb_message = Label(text='Сортировка:')
+        self.button_sort = Button(text='Выполнить')
+        self.button_sort.bind(on_press=self.check_val)
+       
+
+        
+        
+
         
         
         table.add_widget(self.lb_source_choice)
         table.add_widget(btn_source_directory)
-        table.add_widget(self.lb_path)
+        table.add_widget(self.lb_source_empty)
         table.add_widget(self.lb_source_path)
         table.add_widget(self.lb_target_choice)
         table.add_widget(btn_target_directory)
-        
+        table.add_widget(self.lb_target_empty)
+        table.add_widget(self.lb_target_path)
+        table.add_widget(self.lb_message)
+        table.add_widget(self.button_sort) 
+
         layout.add_widget(table)
 
         return layout
 
         #---------------functions----------------------------------------------------------- 
-    def choose_directory(self, instance):
-        root = tk.Tk()
-        root.withdraw()
+    def choose_source_directory(self, instance):
         global directory_path_source
-        directory_path_source = filedialog.askdirectory()
-        cat_dir_path = directory_path_source.split('/')[-1]
-        print(cat_dir_path)
-
-        if cat_dir_path:
-            self.lb_source_path.text = f"{cat_dir_path}"
-            print(f"Выбранный путь: {cat_dir_path}")
-        else:
-            self.lb_source_path.text = f"Выбор отменен!"
-            print("Выбор отменен")
-
-    def choose_directory(self, instance):
         root = tk.Tk()
         root.withdraw()
-        global directory_path
+       
         directory_path_source = filedialog.askdirectory()
         cat_dir_path = directory_path_source.split('/')[-1]
         
@@ -78,7 +84,65 @@ class MyApp(App):
             self.lb_source_path.text = f"Выбор отменен!"
             print("Выбор отменен")
 
+    def choose_target_directory(self, instance):
+        global directory_path_target
+        root = tk.Tk()
+        root.withdraw()
+        
+        directory_path_target = filedialog.askdirectory()
+        cat_dir_path = directory_path_target.split('/')[-1]
+        
+        if cat_dir_path:
+            self.lb_target_path.text = f"{cat_dir_path}"
+            print(directory_path_target)
+            print(f"Выбранный путь: {cat_dir_path}")
+        else:
+            self.lb_target_path.text = f"Выбор отменен!"
+            print("Выбор отменен")
+
+    def check_val(self, instance):
+        global directory_path_source, directory_path_target
+        
+        if directory_path_source and directory_path_target:
+            self.lb_message.text = 'Сортировка:'
+            self.button_sort.text = 'Готово!'
+            # Здесь можно вставить код сортировки
+            self.search_pdf(instance)
+        else:
+            self.lb_message.text = 'Недостаточно данных!'
+
     
+    def search_pdf(self, instance):
+      global directory_path_source
+      global directory_path_target
+      search_text = 'ОСП по Киевскому району г. Симферополя'
+      for root, dirs, files in os.walk(directory_path_source):
+        for filename in files:
+            if filename.lower().endswith('.pdf'):
+                filepath = os.path.join(root, filename)
+                try:
+                    with open(filepath, 'rb') as file:
+                        reader = PyPDF2.PdfReader(file)
+                        # Обход всех страниц
+                        for page_num in range(len(reader.pages)):
+                            page = reader.pages[page_num]
+                            text = page.extract_text()
+                            if text and search_text in text:
+                                print(f"Файл {filepath} содержит искомый текст на странице {page_num + 1}.")
+                                break  # Можно остановить поиск по файлу после нахождения
+                except (PyPDF2.errors.PdfReadError, PermissionError) as e:
+                    print(f"Не удалось прочитать {filepath}: {e}")
+
+        
+       
+
+ 
+
+        
+
+    
+
+    #------------------------------------end functions --------------------------------------------
 
     def app_window_close(self, *args):
        self.stop()
